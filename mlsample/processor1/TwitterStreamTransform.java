158ed261abf11e171fc97f03e8874632d94f7f42
nidhi <nidhimahajan6@gmail.com>
2016-05-20 11:06:44 +0530

    spring XD for real time processing

---
+++ mlsample/processor1/TwitterStreamTransform.java
@@ -0,0 +1,29 @@
+package mlsample.processor1;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
+import org.springframework.integration.transformer.MessageTransformationException;
+
+public class TwitterStreamTransform {
+	
+	private ObjectMapper mapper = new ObjectMapper();
+	
+	public String transform(String payload){
+		try{
+			StringBuilder sb = new StringBuilder();
+			Map<String,Object> tweet = mapper.readValue(payload, new TypeReference<Map<String,Object>>() {
+			});
+			
+			sb.append(tweet.get("created_at").toString());
+			sb.append("|");
+			sb.append(tweet.get("text").toString());
+			return sb.toString();
+		  }catch(IOException e){
+			throw new MessageTransformationException("cannot work on thi tweet");
+		}
+	}
+
+}
